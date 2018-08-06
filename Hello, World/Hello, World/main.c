6a62eedb7ce75c11027f60a2f0d3868a5c298497
yinyongzhen <eagle_01yz@163.com>
2018-08-06 16:03:37 +0800

    运行 C 程序，main(int argc, char *argv[]) 函数传参，argc 为参数个数，argv 是字符串数组， 下标从 0 开始，第一个存放的是可执行程序的文件名字，然后依次存放传入的参数

---
+++ Hello, World/Hello, World/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Hello, World
+//
+//  Created by yongzhen on 2018/8/6.
+//  Copyright © 2018年 yongzhen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("可执行程序 %s, 参数个数 %d, 运行输出 %s\n",argv[0],argc,argv[1]);
+    return 0;
+}
