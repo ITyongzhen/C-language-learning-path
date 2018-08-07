df2d600c1eeead89e43bfb668fbd01f6647e3261
yinyongzhen <eagle_01yz@163.com>
2018-08-07 15:03:28 +0800

    添加笔记

--- Hello, World/Hello, World/main.c
+++ Hello, World/Hello, World/main.c
@@ -7,46 +7,83 @@
 //
 
 #include <stdio.h>
+
 double getAverage(int arr[], int size);
+void getcharAndPutchar();
+void writeOrOpenFile();
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
-    printf("可执行程序 %s, 参数个数 %d, 运行输出 %s\n",argv[0],argc,argv[1]);
-    printf("int 存储大小 %lu \n",sizeof(int));
+//    printf("Hello, World!\n");
+//    printf("可执行程序 %s, 参数个数 %d, 运行输出 %s\n",argv[0],argc,argv[1]);
+//    printf("int 存储大小 %lu \n",sizeof(int));
+//
+//    int arr[5] = {2,43,5,6,23};
+//    double avg;
+//    avg = getAverage(arr, 5);
+//    printf("平均数为 %lf \n",avg);
     
-    int arr[5] = {2,43,5,6,23};
-    double avg;
-    avg = getAverage(arr, 5);
-    printf("平均数为 %lf \n",avg);
-    
-#pragma mark - 指向数组的指针
-    /* 带有 5 个元素的整型数组 */
-    double balance[5] = {1000.0, 2.0, 3.4, 17.0, 50.0};
-    double *p;
-    int i;
-    
-    p = balance;
-    
-    /* 输出数组中每个元素的值 */
-    printf( "使用指针的数组值\n");
-    for ( i = 0; i < 5; i++ )
-    {
-        printf("*(p + %d) : %f\n",  i, *(p + i) );
-    }
-    
-    printf( "使用 balance 作为地址的数组值\n");
-    for ( i = 0; i < 5; i++ )
-    {
-        printf("*(balance + %d) : %f\n",  i, *(balance + i) );
-    }
-    
-    //p 是一个指向 double 型的指针，这意味着它可以存储一个 double 类型的变量。一旦我们有了 p 中的地址，*p 将给出存储在 p 中相应地址的值，正如上面实例中所演示的。
-    
-    
-    
+//    int c;
+//
+//    printf( "Enter a value :");
+//    c = getchar( );
+//
+//    printf( "\nYou entered: ");
+//    putchar( c );
+//    printf( "\n");
+//    getcharAndPutchar;
+//    writeOrOpenFile();
     return 0;
 }
+// nouse
+void writeOrOpenFile(){
+    FILE *p = NULL;
+    p = fopen("/Users/yinyongzhen/Documents/yyz/C语言学", "w+");
+    fprintf(p, "hello ...hello");
+    fputs("fput..", p);
+    fclose(p);
+    
+    
+    
+    
+}
 
+void charArr(){
+#pragma mark - 指向数组的指针
+    {
+        /* 带有 5 个元素的整型数组 */
+        double balance[5] = {1000.0, 2.0, 3.4, 17.0, 50.0};
+        double *p;
+        int i;
+        
+        p = balance;
+        
+        /* 输出数组中每个元素的值 */
+        printf( "使用指针的数组值\n");
+        for ( i = 0; i < 5; i++ )
+        {
+            printf("*(p + %d) : %f\n",  i, *(p + i) );
+        }
+        
+        printf( "使用 balance 作为地址的数组值\n");
+        for ( i = 0; i < 5; i++ )
+        {
+            printf("*(balance + %d) : %f\n",  i, *(balance + i) );
+        }
+        
+        //p 是一个指向 double 型的指针，这意味着它可以存储一个 double 类型的变量。一旦我们有了 p 中的地址，*p 将给出存储在 p 中相应地址的值，正如上面实例中所演示的。
+    }
+}
+void getcharAndPutchar(){
+    int c;
+    
+    printf( "Enter a value :");
+    c = getchar( );
+    
+    printf( "\nYou entered: ");
+    putchar( c );
+    printf( "\n");
+//    return 0;
+}
 double getAverage(int arr[], int size){
     
     int i;
